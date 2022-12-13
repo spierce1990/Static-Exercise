76a704910a5377a4b8f3a8dca35cfbfa8d52a040
Sionylen <sionylenpierce@outlook.com>
2022-12-13 13:30:20 -0600

    added stuff

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace StaticExercise
+﻿using System.ComponentModel;
+
+namespace StaticExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello let us do some temp conversion");
+            Console.WriteLine();
+            Console.WriteLine("Please enter a C you want to convert to F temperature.");
+            var userAnswer1 = Convert.ToDouble( Console.ReadLine());
+
+            
+
+            Console.WriteLine(TempConverter.CelToFar(userAnswer1));
+
+            Console.WriteLine("Please enter a F you want to convert to C temperature.");
+            var userAnswer2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(TempConverter.FarToCel(userAnswer2));
+            
+
+
+            //Now create a static class called TempConverter. The class should have at least 2 methods
+            //one called FahrenheitToCelsius that will require a double as a parameter and return a double,
+            //the other CelsiusToFahrenheit which will also require a double as a parameter and return a double.
+
+            // Fill out these methods and call them in your Program.cs file. Make sure they’re accurate!
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal class TempConverter
+    {
+
+       
+
+        public static  double FarToCel(double a)
+
+        {
+            double newAnswer;
+            newAnswer = (a- 32) * 5 / 9;
+            return newAnswer;
+
+            //F = (C * 9) / 5 + 32;
+           // To convert from Fahrenheit to Celsius: C = (F – 32) *5 / 9;
+        }
+
+    public static double  CelToFar(double b)
+
+        {
+            double newAnswer;
+            newAnswer = (b * 9) / 5 + 32;
+            return newAnswer;
+
+        
+        }
+
+  }
+
+}
